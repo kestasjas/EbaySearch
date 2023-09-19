3f17bccf997926e3e59e5d928836a580f8471e6d
Kestas Jasinevicius <kestasj@comcast.net>
2023-09-19 11:23:20 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.IO;
+using System.Reflection;
+using Xunit;
+
+namespace WebAutoCore
+{
+
+    public class WebTest : WebDriver
+    {
+
+        [Fact]
+        public void IPhoneCheck()
+        {
+
+            NavigateToEbay();
+            SelectIphone();
+            //chrome.Navigate().GoToUrl("https://www.ebay.com/");
+            //chrome.SwitchTo().Frame("callout");
+            //chrome.FindElement(By.ClassName("QlyBfb")).Click();
+
+            QuitChrome();
+
+        }
+
+        //Test Steps
+        void NavigateToEbay() => chrome.Navigate().GoToUrl("https://www.ebay.com/");
+        void SelectIphone()
+        {
+
+        }
+        void CheckResults() { }
+        void OpenAdvertisement() { }
+        void SelectColorQuantity() { }
+        void ClickAdd() { }
+        void CheckPrice() { }
+
+        void QuitChrome() => chrome.Quit();
+    }
+
+    public class TestSteps
+    {
+        
+    }
+
+    public class WebDriver
+    {
+        public IWebDriver chrome;
+        public WebDriver()
+        {
+            chrome = new ChromeDriver(
+               Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), new ChromeOptions());
+            chrome.Manage().Window.Maximize();
+        }
+
+
+    }
+}
